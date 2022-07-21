32a6126dc4e0a7fe36e62aed00728e41e6e9fb3e
daudhiyaa <daud.dhiya31@gmail.com>
2022-07-22 01:21:56 +0700

    updated nav+home

--- Main/Components/Nav+Home/script.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = document.querySelector('.toggle');
-const navigation = document.querySelector('.navigate');
-
-toggleMenu.onclick = function(){
-    toggleMenu.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
